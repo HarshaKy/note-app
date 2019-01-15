d19b613c766faeff9affd745c595bd16f177d2f4
HarshaKy <harshaky6573@gmail.com>
2019-01-15 13:41:34 +0530

    updates

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -8,28 +6,40 @@ const notes = require('./notes.js');
 
 const argv = yargs.argv;
 var command = argv._[0];
-console.log('Command: ', command);
-console.log('Yargs', argv);
 
+// add new note
 if (command === 'add') {
   var note = notes.addNote(argv.title, argv.body);
+
   if (note) {
     console.log('Note created');
     notes.logNote(note);
   } else {
     console.log('Note title taken');
   }
-} else if (command === 'list') {
-  notes.getAll();
-} else if (command === 'read') {
+}
+
+// list notes
+ else if (command === 'list') {
+  var allNotes = notes.getAll();
+  console.log(`Printing ${allNotes.length} note(s).`);
+  allNotes.forEach((note) => notes.logNote(note));
+}
+
+// read a note
+ else if (command === 'read') {
   var note = notes.getNote(argv.title);
+
   if (note) {
     console.log('Note found');
     notes.logNote(note);
   } else {
     console.log('Note not found');
   }
-} else if (command === 'remove') {
+}
+
+// remove a note
+ else if (command === 'remove') {
   var noteRemoved = notes.removeNote(argv.title);
   var message = noteRemoved ? 'Note was removed' : 'Note not found';
   console.log(message);
